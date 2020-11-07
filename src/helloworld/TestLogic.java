d1e9963ccd00eb98c6083ea55185547ea8c265e9
gitGood6969 <72687984+gitGood6969@users.noreply.github.com>
2020-11-07 18:00:42 +0800

    base files

---
+++ src/helloworld/TestLogic.java
@@ -0,0 +1,22 @@
+package helloworld;
+
+import static org.junit.Assert.*;
+//import com.javatpoint.logic.*;
+import org.junit.Test;
+
+public class TestLogic {
+
+    
+    @Test
+    public static void testFindMax(){
+            assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));
+            //assertEquals(-2,Calculation.findMax(new int[]{-12,-3,-4,-2}));
+    }
+
+    public static void main(String[] args) 
+    {
+        testFindMax();
+    }
+}
+// https://www.javatpoint.com/junit-tutorial
+// https://condor.depaul.edu/glancast/383class/docs/junit_netbeans.html
