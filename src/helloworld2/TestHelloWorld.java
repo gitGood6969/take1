93ba8337c69968ec10a8d61b925d15b4828d71d6
gitGood6969 <72687984+gitGood6969@users.noreply.github.com>
2020-11-09 18:38:02 +0800

    take 4

--- src/helloworld2/TestHelloWorld.java
+++ src/helloworld2/TestHelloWorld.java
@@ -1,15 +1,33 @@
 // Package
 package helloworld2;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestHelloWorld extends junit.framework.TestCase 
 {
-    public void testNothing() 
-    {
+   private helloworld2.HelloWorld2 h;
 
-    }
+   @Before
+   public void setUp() throws Exception
+   {
+      h = new helloworld2.HelloWorld2();
+   }
 
-    public void testWillAlwaysFail() 
-    {
-        fail("An error message");
-    }
+   @Test
+   public void testHelloEmpty()
+   {
+      assertEquals(h.getName(),"");
+      assertEquals(h.getMessage(),"Hello!");
+   }
+
+   @Test
+   public void testHelloWorld()
+   {
+      h.setName("World");
+      assertEquals(h.getName(),"World");
+      assertEquals(h.getMessage(),"Hello World!");
+   }
 }
