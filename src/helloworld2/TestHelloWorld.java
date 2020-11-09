f925ab832236039dc77bdfa008886aa5ae5d7ace
gitGood6969 <72687984+gitGood6969@users.noreply.github.com>
2020-11-09 17:36:14 +0800

    now with JUNIT

---
+++ src/helloworld2/TestHelloWorld.java
@@ -0,0 +1,15 @@
+// Package
+package helloworld2;
+
+public class TestHelloWorld extends junit.framework.TestCase 
+{
+    public void testNothing() 
+    {
+
+    }
+
+    public void testWillAlwaysFail() 
+    {
+        fail("An error message");
+    }
+}
